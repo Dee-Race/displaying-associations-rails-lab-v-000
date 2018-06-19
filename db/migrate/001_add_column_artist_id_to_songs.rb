97e2cb6313ae8ed0d420ad712ee53dca4038d0fb
Dee Race <deerace12@gmail.com>
2018-06-19 15:58:20 +0000

    automatically backed up by learn

--- db/migrate/001_add_column_artist_id_to_songs.rb
+++ db/migrate/001_add_column_artist_id_to_songs.rb
@@ -1,5 +1,5 @@
-class AddColumnArtistIdToSongs < ActiveRecord::Migration 
-  def change 
-    add_column :songs, :artist_id, :integer 
-  end 
+class AddColumnArtistIdToSongs < ActiveRecord::Migration
+  def change
+    add_column :songs, :artist_id, :integer
+  end
 end
